f9cb285a391d1a730915da083bcbd74dc22eeeb2
Joseph Dykstra <josephdykstra@gmail.com>
2017-01-21 13:02:23 -0600

    Use spaces instead of tabs

--- collision-checking.js
+++ collision-checking.js
@@ -1,12 +1,12 @@
 module.exports = function makeCollisionCheckingFunction(level, collisionTiles) {
-	var levelHeight = level.length
-	var levelWidth = level[0].length
+    var levelHeight = level.length
+    var levelWidth = level[0].length
 
-	return function checkForCollision(row, col) {
-		// http://stackoverflow.com/questions/4228356/integer-division-in-javascript
-		row = Math.floor((row + levelHeight) % levelHeight)
-		col = Math.floor((col + levelWidth)  % levelWidth)
+    return function checkForCollision(row, col) {
+        // http://stackoverflow.com/questions/4228356/integer-division-in-javascript
+        row = Math.floor((row + levelHeight) % levelHeight)
+        col = Math.floor((col + levelWidth)  % levelWidth)
 
-		return collisionTiles.indexOf(level[row][col]) !== -1
-	}
+        return collisionTiles.indexOf(level[row][col]) !== -1
+    }
 }
